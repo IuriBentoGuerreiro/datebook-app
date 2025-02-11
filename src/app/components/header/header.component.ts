000c9bd00cc58f9d9a35b65c3ccca3d0d55a2cd6
iuri <bentoiuri@gmail.com>
2025-02-11 17:46:26 -0300

    adiciona dropdown

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,20 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MatCommonModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatIcon } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIcon, CommonModule],
+  imports: [
+    MatIcon, 
+    CommonModule,
+    MatButtonModule,
+    MatMenuModule,
+    RouterModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
-  menuOpen = false;
+  isMenuVisible = false;
 
   toggleMenu() {
-    this.menuOpen = !this.menuOpen;
+    this.isMenuVisible = !this.isMenuVisible;
   }
 }
