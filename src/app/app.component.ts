ea21784906d332c805e530d360dc554d62751723
iuri <bentoiuri@gmail.com>
2025-04-16 10:41:00 -0300

    ajusta responsividade de register e login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Event, NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { LoadingComponent } from "./components/loading/loading.component";
+import { filter } from 'rxjs';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoadingComponent, HeaderComponent],
+  imports: [RouterOutlet, LoadingComponent, HeaderComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'datebook-app';
+
+  showHeader = true;
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(
+        filter((event: Event): event is NavigationEnd => event instanceof NavigationEnd)
+      )
+      .subscribe((event) => {
+        this.showHeader = !['/login', '/register'].some(path =>
+          event.urlAfterRedirects.includes(path)
+        );
+      });
+  }
+  
 }
